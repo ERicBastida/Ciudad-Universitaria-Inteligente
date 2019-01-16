41fa4c959d56ed0c5a0f003d25027e4220c9a273
ERicBastida <emb_mario@hotmail.com>
2019-01-16 18:35:49 -0300

    Agregada clase loggin

---
+++ app/src/main/java/com/eric/tpfinal/SpinnerAdapter.java
@@ -0,0 +1,55 @@
+package com.eric.tpfinal;
+
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import java.util.ArrayList;
+
+public class SpinnerAdapter extends ArrayAdapter<ItemData> {
+    int groupid;
+    Activity context;
+    ArrayList<ItemData> list;
+    LayoutInflater inflater;
+    Activity actividad;
+
+    LogginCUI log = new LogginCUI();
+
+    public SpinnerAdapter(Activity context, int groupid, int id, ArrayList<ItemData>  list){
+        super(context,id,list);
+        this.actividad = context;
+        this.list=list;
+        inflater=(LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        this.groupid=groupid;
+
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent ){
+
+        View itemView=inflater.inflate(groupid,parent,false);
+        try {
+            ImageView imageView = (ImageView) itemView.findViewById(R.id.imgItemSpinner);
+            imageView.setImageResource(list.get(position).getImageId());
+            //        TextView textView=(TextView)itemView.findViewById(R.id.txtItemSpinner);
+            TextView textView = (TextView) itemView.findViewById(R.id.adjust_width);
+            textView.setText(list.get(position).getText());
+        }catch (Exception e){
+
+            log.registrar(this,"getView",e);
+            log.alertar("Ha ocurrido un error en la funcion getView",actividad);
+
+        }
+        return itemView;
+    }
+
+    public View getDropDownView(int position, View convertView, ViewGroup
+            parent){
+        return getView(position,convertView,parent);
+
+    }
+}
